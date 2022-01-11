a54bcd3a600e3f26e76f554645fc8344a4255e88
Nikolay Shayahmetov <shayahmetov@gmail.com>
2022-01-11 17:06:10 +0500

    Update CSS archive

--- views/order/archive.php
+++ views/order/archive.php
@@ -78,130 +78,218 @@ $gridColumns = [
 ];
 ?>
 <div class="box box-primary">
-
-    <div class="box-body">
-        <?php
-
-        // Renders a export dropdown menu
-        echo ExportMenu::widget([
+    <div class="box-header with-border">
+        <?= ExportMenu::widget([
             'dataProvider' => $dataProvider,
             'columns' => $gridColumns
         ]);
         ?>
-
-        <?= GridView::widget([
-            'dataProvider' => $dataProvider,
-            'filterModel' => $searchModel,
-            'rowOptions'=>function (\app\models\Order  $model, $key, $index, $grid) {
-                return [
-                    'class'=> $model->statusColor
-                ];
-            },
-            'columns' => [
-                [
-                    'attribute' => 'id',
-                    'options' => ['width' => 50]
-                ],
-                [
-                    'filter' => DateRangePicker::widget([
-                        'model' => $searchModel,
-                        'attribute' => 'date_range',
-                        'startAttribute' => 'date_from',
-                        'endAttribute' => 'date_to',
-                        'convertFormat'=>true,
-                        'options' => ['placeholder' => 'Выберите даты...', 'id' => 'date_range', 'class' => 'form-control'],
-                        'pluginEvents' => [
-                            "cancel.daterangepicker" => "function(ev, picker) {
+    </div>
+    <div class="box-body">
+        <div class="hidden-xs">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'rowOptions'=>function (\app\models\Order  $model, $key, $index, $grid) {
+                    return [
+                        'class'=> $model->statusColor
+                    ];
+                },
+                'columns' => [
+                    [
+                        'attribute' => 'id',
+                        'options' => ['width' => 50]
+                    ],
+                    [
+                        'filter' => DateRangePicker::widget([
+                            'model' => $searchModel,
+                            'attribute' => 'date_range',
+                            'startAttribute' => 'date_from',
+                            'endAttribute' => 'date_to',
+                            'convertFormat'=>true,
+                            'options' => ['placeholder' => 'Выберите даты...', 'id' => 'date_range', 'class' => 'form-control'],
+                            'pluginEvents' => [
+                                "cancel.daterangepicker" => "function(ev, picker) {
                             var poleDate = picker.element[0].nextElementSibling;
                             $(date_range).val('').trigger('change');
                             }",
-                        ],
-                        'pluginOptions'=>[
-                            'locale'=>['format'=>'Y-m-d',  'cancelLabel' => 'Очистить']
-                        ]
-                    ]),
-                    'attribute' => 'created_at',
-                    'format' =>  ['date', 'php:d M Y H:i'],
-                    'options' => ['width' => '185']
-                ],
-                [
-                    'attribute' => 'equipment_kind',
-                    'content' => function (Order $model) {
-                        return $model->equipment->kind;
-                    }
-                ],
-                [
-                    'attribute' => 'equipment_brand',
-                    'content' => function (Order $model) {
-                        return $model->equipment->brand;
-                    }
-                ],
-                [
-                    'attribute' => 'equipment_sample',
-                    'content' => function ($model) {
-                        return $model->equipment->sample;
-                    }
-                ],
-                [
-                    'attribute' => 'equipment_serial_number',
-                    'content' => function (Order $model) {
-                        return $model->equipment->serial_number;
-                    }
-                ],
-                [
-                    'attribute' => 'client_fio',
-                    'filter' => Select2::widget([
-                        'model' => $searchModel,
-                        'attribute' => 'client_fio',
-                        'initValueText' => ($searchModel->client) ? $searchModel->client->fio : '',
-                        'theme' => Select2::THEME_BOOTSTRAP,
-                        'options' => [
-                            'placeholder' => 'ФИО клиента',
-                            'multiple' => false,
-                        ],
-                        'hideSearch' => false,
-                        'pluginOptions' => [
-                            'tags' => true,
-                            'allowClear' => true,
-                            'minimumInputLength' => 2,
-                            'language' => [
-                                'errorLoading' => new JsExpression ("function () { return 'Поиск клиента...'; }"),
                             ],
-                            'ajax' => [
-                                'url' => Url::to(['client/ajax-get']),
-                                'dataType' => 'json',
-                                'data' => new JsExpression('function(params) { return {q:params.term}; }')
-                            ],
-                        ],
-                    ]),
-                    'content' => function (\app\models\Order $model) {
-                        return $model->client->fio;
-                    }
-                ],
-                [
-                    'attribute' => 'client_phone',
-                    'content' => function (\app\models\Order $model) {
-                        return $model->client->phoneFormat;
-                    }
-                ],
-                'comment',
-                [
-                    'class' => 'yii\grid\ActionColumn',
-                    'template' => '{update}',
-                    'buttons' => [
-                        'print' => function($url,$model,$key){
-                            return Html::a('<span class="glyphicon glyphicon-print"></span>',
-                                ['print/order', 'id' => $model->id],
-                                ['onclick' => "window.open(this.href, 'Печать Акта', 'height=600, width=800,toolbar=0,location=0,menubar=0'); return false;"]);
+                            'pluginOptions'=>[
+                                'locale'=>['format'=>'Y-m-d',  'cancelLabel' => 'Очистить']
+                            ]
+                        ]),
+                        'attribute' => 'created_at',
+                        'format' =>  ['date', 'php:d M Y H:i'],
+                        'options' => ['width' => '185']
+                    ],
+                    [
+                        'attribute' => 'equipment_kind',
+                        'content' => function (Order $model) {
+                            return $model->equipment->kind;
                         }
-                    ]
+                    ],
+                    [
+                        'attribute' => 'equipment_brand',
+                        'content' => function (Order $model) {
+                            return $model->equipment->brand;
+                        }
+                    ],
+                    [
+                        'attribute' => 'equipment_sample',
+                        'content' => function ($model) {
+                            return $model->equipment->sample;
+                        }
+                    ],
+                    [
+                        'attribute' => 'equipment_serial_number',
+                        'content' => function (Order $model) {
+                            return $model->equipment->serial_number;
+                        }
+                    ],
+                    [
+                        'attribute' => 'client_fio',
+                        'filter' => Select2::widget([
+                            'model' => $searchModel,
+                            'attribute' => 'client_fio',
+                            'initValueText' => ($searchModel->client) ? $searchModel->client->fio : '',
+                            'theme' => Select2::THEME_BOOTSTRAP,
+                            'options' => [
+                                'placeholder' => 'ФИО клиента',
+                                'multiple' => false,
+                            ],
+                            'hideSearch' => false,
+                            'pluginOptions' => [
+                                'tags' => true,
+                                'allowClear' => true,
+                                'minimumInputLength' => 2,
+                                'language' => [
+                                    'errorLoading' => new JsExpression ("function () { return 'Поиск клиента...'; }"),
+                                ],
+                                'ajax' => [
+                                    'url' => Url::to(['client/ajax-get']),
+                                    'dataType' => 'json',
+                                    'data' => new JsExpression('function(params) { return {q:params.term}; }')
+                                ],
+                            ],
+                        ]),
+                        'content' => function (\app\models\Order $model) {
+                            return $model->client->fio;
+                        }
+                    ],
+                    [
+                        'attribute' => 'client_phone',
+                        'content' => function (\app\models\Order $model) {
+                            return $model->client->phoneFormat;
+                        }
+                    ],
+                    'comment',
+                    [
+                        'class' => 'yii\grid\ActionColumn',
+                        'template' => '{update}',
+                        'buttons' => [
+                            'print' => function($url,$model,$key){
+                                return Html::a('<span class="glyphicon glyphicon-print"></span>',
+                                    ['print/order', 'id' => $model->id],
+                                    ['onclick' => "window.open(this.href, 'Печать Акта', 'height=600, width=800,toolbar=0,location=0,menubar=0'); return false;"]);
+                            }
+                        ]
+                    ],
+
+
+
                 ],
+            ]); ?>
+
+
+        </div>
+        <div class="visible-xs table-responsive">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'rowOptions'=>function (\app\models\Order  $model, $key, $index, $grid) {
+                    return [
+                        'class'=> $model->statusColor
+                    ];
+                },
+                'columns' => [
+                    [
+                        'attribute' => 'id',
+                        'options' => ['width' => 50]
+                    ],
+                    [
+                        'filter' => DateRangePicker::widget([
+                            'model' => $searchModel,
+                            'attribute' => 'date_range',
+                            'startAttribute' => 'date_from',
+                            'endAttribute' => 'date_to',
+                            'convertFormat'=>true,
+                            'options' => ['placeholder' => 'Выберите даты...', 'id' => 'date_range', 'class' => 'form-control'],
+                            'pluginEvents' => [
+                                "cancel.daterangepicker" => "function(ev, picker) {
+                            var poleDate = picker.element[0].nextElementSibling;
+                            $(date_range).val('').trigger('change');
+                            }",
+                            ],
+                            'pluginOptions'=>[
+                                'locale'=>['format'=>'Y-m-d',  'cancelLabel' => 'Очистить']
+                            ]
+                        ]),
+                        'attribute' => 'created_at',
+                        'format' =>  ['date', 'php:d M Y H:i'],
+                        'options' => ['width' => '185']
+                    ],
+                    [
+                        'attribute' => 'equipment_serial_number',
+                        'content' => function (Order $model) {
+                            return $model->equipment->kind. ' '. $model->equipment->brand. ' '. $model->equipment->sample.
+                                Html::tag('span', $model->equipment->serial_number, ['class' => 'text-muted']);
+                        }
+                    ],
+
+                    [
+                        'attribute' => 'client_fio',
+                        'filter' => Select2::widget([
+                            'model' => $searchModel,
+                            'attribute' => 'client_fio',
+                            'initValueText' => ($searchModel->client) ? $searchModel->client->fio : '',
+                            'theme' => Select2::THEME_BOOTSTRAP,
+                            'options' => [
+                                'placeholder' => 'ФИО клиента',
+                                'multiple' => false,
+                            ],
+                            'hideSearch' => false,
+                            'pluginOptions' => [
+                                'tags' => true,
+                                'allowClear' => true,
+                                'minimumInputLength' => 2,
+                                'language' => [
+                                    'errorLoading' => new JsExpression ("function () { return 'Поиск клиента...'; }"),
+                                ],
+                                'ajax' => [
+                                    'url' => Url::to(['client/ajax-get']),
+                                    'dataType' => 'json',
+                                    'data' => new JsExpression('function(params) { return {q:params.term}; }')
+                                ],
+                            ],
+                        ]),
+                        'content' => function (Order $model) {
+                            return $model->client->fio.' <br>'.$model->client->phoneFormat;
+                        }
+                    ],
+
+                    'comment',
+                    [
+                        'class' => 'yii\grid\ActionColumn',
+                        'template' => '{update}',
+
+                    ],
 
 
 
-            ],
-        ]); ?>
+                ],
+            ]); ?>
 
+        </div>
 
     </div>
 </div>
